58bde1d7f0a9afc2bd8e0be4f1a857f85e0c4b83
felix <felix@localhost.localdomain>
2017-10-21 23:31:03 +0200

    Bug fixes and smoothening

--- kompaktstar.py
+++ kompaktstar.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pylab
 from scipy.integrate import quad
 from matplotlib import pyplot as plt
 
@@ -6,23 +7,27 @@ from matplotlib import pyplot as plt
 # Definiere Konstanten (Einheiten im cgs-System)
 rho_c = 2.0*10**14	# zentrale Dichte
 G = 6.674*10**(-8)	# Gravitationskonstange
+c = 29979245800.0	# Lichtgeschwindigkeit
 K = 5.3802*10**9	# aus Shapiro, Teukolsky, nicht-relativistisches Neutronengas (nrel-ngas)
 A = 2./3.*np.pi*G 	# mehrmals verwendete Konstante
 g = 5./3.		# Gamma der Zustandsgleichung (EOS) p=rho^Gamma für nrel-ngas	
 b = 3./5.		# 1/Gamma
 
 # Berechne Druck im Zentrum mit EOS
-rho=rho_c;
-p_c=K*rho_c**g;
+rho=rho_c
+p_c=K*rho_c**g
 
-#Schrittweite der Integration nach r (in cm)
-dr = 10.0;
+# Schrittweite der Integration nach r (in cm)
+dr = 100.0
 
 # Definiere Funktion zur Berechnung von dm und dp (Sternstruktur-Gleichungen)
 def mass(r, d):
     return 4.0*np.pi*r**2.0*d
 def press(r, m, d):
     return (1/(r**2.0))*(-G*m*d)
+# dimensionslose Grösse
+def dimless(M, r):
+    return 1-(2.0*G*M)/(c**2.0*r)
 
 # 1. Integrationsschritt von r=0 nach r=dr mittels Näherung rho=rho_c (aus Vorlesung [26.9.17])
 p = p_c-rho_c**2.0*A*dr**2.0
@@ -33,12 +38,13 @@ M = 0.0
 rvec = [0.0, dr]
 pvec = [p_c,p]
 mvec = [0.0, 4./3.*np.pi*dr**3.0*rho_c]
-rhovec = [rho_c, rho]
+rhovec = [rho_c,rho]
 
-n=2.0	#n. Integrationsschritt
+n=2.0	# n. Integrationsschritt
 
 # numerische Integration zur schrittweise Berechnung von rho(r) und p(r)
-while True:
+while p>0:
+    rho = (p/K)**b
     d = rho
     # berechne Masse der n-ten Massenschale durch Integration
     m = quad(mass,(n-1.0)*dr,n*dr,args=(d))
@@ -49,31 +55,47 @@ while True:
     # neuer Druck berechnen
     p = p+P[0]
     # neu berechneter Druckwert p an Vektor anhängen
-    pvec = np.append(pvec,p)
+    pvec = np.append(pvec, p)
     # r-Vektor um r+dr erweitern
-    rvec = np.append(rvec,rvec[-1]+dr)
+    rvec = np.append(rvec, rvec[-1]+dr)
     # m-Vektor um aktuelle Masse erweitern
-    mvec = np.append(mvec,M)
+    mvec = np.append(mvec, M)
     # neu berechnete Dichte rho an Vektor anhängen
     rhovec = np.append(rhovec, rho)
-    if p<0:
-        break 
-    rho =(p/K)**b
     #Integrationsschritt n um 1 erhöhen
     n += 1	
 
-# Daten in txt-Datei abspeichern
-data = np.array([rvec,rhovec,pvec,mvec])
-data = data.T
-np.savetxt('data.txt',data)
+# Dimensionslose Grösse 1-2Gm/(c^2*r) berechnen für r = 0-19 km in 1km-Schritten
+dlvec = [1]
+rrvec = [0]
+for i in range(1, 20):
+    a = i*1000
+    dl = dimless(mvec[a], rvec[a])
+    dlvec = np.append(dlvec, dl)
+    rrvec = np.append(rrvec, rrvec[-1]+1)
 
-# Plotte das Dichte-Profil
-plt.xlabel('Radius r')
-plt.ylabel('Dichte rho')
-plt.title('Dichte-Profil Neutronenstern')
-plt.plot(rvec, rhovec)
-plt.show()
-plt.savefig('plot.png')
+
+# Daten in txt-Datei abspeichern
+data = np.array([rvec, rhovec, pvec, mvec])
+data = data.T
+np.savetxt('data.txt', data)
+np.savetxt('dimless.txt', dlvec)
+
+# Plot des Dichte-Profils
+plt.figure(0)
+plt.xlabel(r"Radius $r$ [km]")
+plt.ylabel(r"Dichte $\rho$ [$\cdot 10^{14}\frac{g}{cm^3}$]")
+plt.title('Dichte-Profil des Neutronensterns')
+plt.plot(1.0*10**(-5)*rvec, 1.0*10**(-14)*rhovec) # von cm --> km
+pylab.savefig('plot.png', dpi=300)
+
+# Plot die dimensionslose Grösse 1-2Gm/(c^2*r)
+plt.figure(1)
+plt.xlabel(r"Radius $r$ [km]")
+plt.ylabel(r"$1-\frac{2Gm}{c^2r}$")
+plt.title('Dichte-Profil des Neutronensterns')
+plt.plot(rrvec, dlvec)
+pylab.savefig('dimless.png', dpi=300)
 
 
     
